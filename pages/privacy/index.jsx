6b2ee77528c5d86849a74f187eb0010740d94319
Your Name <you@example.com>
2023-03-30 17:37:18 +0300

    new page

--- pages/privacy/index.jsx
+++ pages/privacy/index.jsx
@@ -14,16 +14,15 @@ export default function Privacy() {
 The use of this website is subject to the following terms of use:
 </p>
 
-<h2>
+<p><h2>
 Cookies 
-</h2><p>
+</h2>
 We do not use "cookies" on classics.tv. 
 </p><p>
 Some of our business partners may use cookies on our site (for example, advertisers). However, we have no access to or control over these cookies. 
 </p><p>
-Collecting Data 
-Google Analytics is a web analysis service provided by Google. Google utilizes the data collected to track and examine the use of classics.tv.
-</p>
+<h2>Collecting Data</h2>
+Google Analytics is a web analysis service provided by Google. Google utilizes the data collected to track and examine the use of classics.tv.</p>
     </div>
   )
 }
