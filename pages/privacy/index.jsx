05800425ddb431d90b5b9dd802bd05e2c12b0c49
Yosif Georgiev <iodesign@live.com>
2023-03-30 18:26:25 +0300

    UPDATE: CSS Privacy

--- pages/privacy/index.jsx
+++ pages/privacy/index.jsx
@@ -1,28 +1,28 @@
-import styles from './Privacy.module.css'
+import styles from "./Privacy.module.css";
 
 export default function Privacy() {
-
-
-
-
   return (
     <div className={styles.container}>
-    <h1 className={styles.pageInfoWrapper}>Privacy Policy</h1>
-   
-        
-<p>
-The use of this website is subject to the following terms of use:
-</p>
-
-<p><h2>
-Cookies 
-</h2>
-We do not use "cookies" on classics.tv. 
-</p><p>
-Some of our business partners may use cookies on our site (for example, advertisers). However, we have no access to or control over these cookies. 
-</p><p>
-<h2>Collecting Data</h2>
-Google Analytics is a web analysis service provided by Google. Google utilizes the data collected to track and examine the use of classics.tv.</p>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <h1>Privacy Policy</h1>
+          <p>
+            The use of this website is subject to the following terms of use:
+          </p>
+          <h2>Cookies</h2>
+          <p>We do not use "cookies" on classics.tv.</p>
+          <p>
+            Some of our business partners may use cookies on our site (for
+            example, advertisers). However, we have no access to or control over
+            these cookies.
+          </p>
+          <p>
+            Collecting Data Google Analytics is a web analysis service provided
+            by Google. Google utilizes the data collected to track and examine
+            the use of classics.tv.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
