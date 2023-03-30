fb5175c09a728f50a4975892db6f19293a30e92e
Yosif Georgiev <iodesign@live.com>
2023-03-30 18:26:25 +0300

    UPDATE: CSS Privacy

--- pages/terms/index.jsx
+++ pages/terms/index.jsx
@@ -1,23 +1,35 @@
-import styles from './Terms.module.css'
+import styles from "./Terms.module.css";
 
 export default function Terms() {
-
-
-
-
   return (
     <div className={styles.container}>
-    <h1 className={styles.pageInfoWrapper}>Terms of Service</h1>
-   
-        
-<p>
-The use of this website is subject to the following terms of use:
-</p><p>
-The content of the pages of this website is for your general information and use only. It is subject to change without notice.
-This website doen not use cookies to monitor browsing preferences.</p><p>
-Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law.</p><p>
-Your use of any information or materials on this website is entirely at your own risk, for which we shall not be liable. It shall be your own responsibility to ensure that any products, services or information available through this website meet your specific requirements.</p><p>
-All trade marks reproduced in this website which are not the property of, or licensed to, the operator are acknowledged on the website.</p>
+      <h1 className={styles.pageInfoWrapper}>Terms of Service</h1>
+
+      <p>The use of this website is subject to the following terms of use:</p>
+      <p>
+        The content of the pages of this website is for your general information
+        and use only. It is subject to change without notice. This website doen
+        not use cookies to monitor browsing preferences.
+      </p>
+      <p>
+        Neither we nor any third parties provide any warranty or guarantee as to
+        the accuracy, timeliness, performance, completeness or suitability of
+        the information and materials found or offered on this website for any
+        particular purpose. You acknowledge that such information and materials
+        may contain inaccuracies or errors and we expressly exclude liability
+        for any such inaccuracies or errors to the fullest extent permitted by
+        law.
+      </p>
+      <p>
+        Your use of any information or materials on this website is entirely at
+        your own risk, for which we shall not be liable. It shall be your own
+        responsibility to ensure that any products, services or information
+        available through this website meet your specific requirements.
+      </p>
+      <p>
+        All trade marks reproduced in this website which are not the property
+        of, or licensed to, the operator are acknowledged on the website.
+      </p>
     </div>
-  )
+  );
 }
