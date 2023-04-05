2463f9ffa7dccc2c9ec010859b320de4e1c5a8ed
Yosif Georgiev <iodesign@live.com>
2023-04-05 20:23:16 +0300

    NEW: add Modal

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import "../styles/globals.css";
 import Layout from "@/components/layout/Layout";
 
@@ -6,16 +8,20 @@ import SEO from "../next-seo.config";
 
 import { Roboto } from "next/font/google";
 
+
 const roboto = Roboto({
   weight: ["100", "300", "400", "500", "700", "900"],
   subsets: ["latin"],
 });
 
 export default function App({ Component, pageProps }) {
+
+
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <>
       <DefaultSeo {...SEO} />
-      <Layout className={roboto.className}>
+      <Layout className={roboto.className}>     
         <Component {...pageProps} />
       </Layout>
     </>
