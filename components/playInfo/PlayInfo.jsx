08d814ebf501f6a93b106656ec15d99456427aad
Yosif Georgiev <iodesign@live.com>
2023-04-05 17:33:18 +0300

    UPDATE: Play Info with loading status

--- components/playInfo/PlayInfo.jsx
+++ components/playInfo/PlayInfo.jsx
@@ -3,12 +3,20 @@ import styles from "./PlayInfo.module.css";
 export default function PlayInfo({ title, jsonLength, year, channelInfo }) {
   return (
     <div className={styles.playInfoContainer}>
-      <h4>
-        Now playing:<span> {title}</span>
-      </h4>
-      <p>
-        Year: <span>{year}</span>
-      </p>
+      {title ? (
+        <h4>
+          Now playing:<span>{title}</span>
+        </h4>
+      ) : (
+        <h4>Loading Title...</h4>
+      )}
+      {year ? (
+        <p>
+          Year: <span>{year}</span>
+        </p>
+      ) : (
+        ""
+      )}
       <h5>
         Available channels: <span>{jsonLength}</span>
       </h5>
