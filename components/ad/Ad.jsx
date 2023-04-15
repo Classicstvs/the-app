f087a687cbb46d414a42a0460caed201667ee44d
Yosif Georgiev <iodesign@live.com>
2023-04-15 22:02:05 +0300

    FIX: google adsense

--- components/ad/Ad.jsx
+++ components/ad/Ad.jsx
@@ -1,11 +1,21 @@
+import { useEffect } from "react";
 import styles from "./Ad.module.css";
 
 export default function Ad() {
+  useEffect(() => {
+    var ads = document.getElementsByClassName("adsbygoogle").length;
+      for (var i = 0; i < ads; i++) {
+        try {
+          (adsbygoogle = window.adsbygoogle || []).push({});
+        } catch (e) { }
+      }
+  }, []);
+
   return (
     <div className={styles.container}>
       <ins
-        class="adsbygoogle"
-        style="display:block"
+        className="adsbygoogle"
+        style={{ display: "block" }}
         data-ad-client="ca-pub-4275984189085881"
         data-ad-slot="3375378818"
         data-ad-format="auto"
--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { useEffect } from "react";
 
 export default function Document() {
-  useEffect(()=>{
-    var ads = document.getElementsByClassName("adsbygoogle").length;
-    for (var i = 0; i < ads; i++) {
-      try {
-        (adsbygoogle = window.adsbygoogle || []).push({});
-      } catch (e) { }
-    }
-  })
   return (
     <Html lang="en">
       <Head>
@@ -33,7 +24,6 @@ export default function Document() {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4275984189085881"
           crossorigin="anonymous"
         ></script>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       </Head>
       <body>
         <Main />
