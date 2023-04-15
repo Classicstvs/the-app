90b9a8436cb8820e1bff04541b3d1d1f8859fda5
Yosif Georgiev <iodesign@live.com>
2023-04-15 20:28:25 +0300

    NEW: google script

--- pages/_document.js
+++ pages/_document.js
@@ -8,17 +8,22 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
         {/* <link rel="preload" /> */}
         <meta name="theme-color" content="#33333" />
-        <meta
-          property="og:image"
-          content="/images/og_image.png"
-        />
+        <meta property="og:image" content="/images/og_image.png" />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="228" />
         <meta property="og:image:height" content="122" />
-        <meta name="description" content="Relive the golden age of television with our classic TV channels. Our vintage collection will transport you back in time. Tune in and enjoy the best from the 50s, 60s, 70s, 80s, 90s, and 2000s." />
+        <meta
+          name="description"
+          content="Relive the golden age of television with our classic TV channels. Our vintage collection will transport you back in time. Tune in and enjoy the best from the 50s, 60s, 70s, 80s, 90s, and 2000s."
+        />
         <meta property="og:title" content="Classics TV" />
         <meta property="og:url" content="www.classicstv.net" />
         <meta property="og:type" content="website" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4275984189085881"
+          crossorigin="anonymous"
+        ></script>
       </Head>
       <body>
         <Main />
