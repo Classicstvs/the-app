7f1a85b89ed0d9cfed0b77670a229af27f0d0e0b
Yosif Georgiev <iodesign@live.com>
2023-04-15 21:27:21 +0300

    NEW: Adsense

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { useEffect } from "react";
 
 export default function Document() {
+  useEffect(()=>{
+    var ads = document.getElementsByClassName("adsbygoogle").length;
+    for (var i = 0; i < ads; i++) {
+      try {
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      } catch (e) { }
+    }
+  })
   return (
     <Html lang="en">
       <Head>
@@ -24,6 +33,7 @@ export default function Document() {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4275984189085881"
           crossorigin="anonymous"
         ></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       </Head>
       <body>
         <Main />
