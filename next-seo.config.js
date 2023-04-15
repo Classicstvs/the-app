b2149c2983fe462fcb2f0235e15845f0028c8cb1
viktor0980 <viktor0980@gmail.com>
2023-04-15 12:58:34 +0300

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     title: "Classics TV",
-    description: "Relive the golden age of television with our classic TV channels from the past. Big collection of vintage TV channels will transport you back in time. Tune in to our channels and enjoy the best of classic TV from the 50s, 60s, 70s, 80s, 90s, and 2000s.",
+    description: "Relive the golden age of television with our classic TV channels. Our vintage collection will transport you back in time. Tune in and enjoy the best from the 50s, 60s, 70s, 80s, 90s, and 2000s.",
     openGraph:{
         locale: 'us_US',
         site_name: 'Classics TV',
