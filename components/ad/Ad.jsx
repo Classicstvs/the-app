8fda8fc1209d8da0b3b0f0652a20dced9a19feaf
Yosif Georgiev <iodesign@live.com>
2023-04-15 21:27:21 +0300

    NEW: Adsense

--- components/ad/Ad.jsx
+++ components/ad/Ad.jsx
@@ -1,9 +1,16 @@
-import styles from './Ad.module.css'
+import styles from "./Ad.module.css";
 
 export default function Ad() {
   return (
     <div className={styles.container}>
-        <p>Just a little empty white space.</p>
+      <ins
+        class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-4275984189085881"
+        data-ad-slot="3375378818"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
     </div>
-  )
+  );
 }
