794289ff55164df2b469e60c1983e78219d43911
Yosif Georgiev <iodesign@live.com>
2023-04-18 16:19:16 +0300

    FIX: ad1

--- components/ad/Ad.jsx
+++ components/ad/Ad.jsx
@@ -8,9 +8,9 @@ export default function Ad() {
   return (
     <div className={styles.container}>
       <Adsense
-        style={{height:'164px' }}
+        style={{height:'164px'}}
         client="ca-pub-4803482388154607"
-        slot="3375378818"
+        slot="2382819204"
         // format="auto"
         responsive="true"
       />
